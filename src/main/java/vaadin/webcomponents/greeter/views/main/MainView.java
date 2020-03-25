5fd4c1260c22191448b0830aed0e3735945a63ba
Tobias Waller <tobias.waller@capgemini.com>
2020-03-25 14:56:08 +0100

    adds vaadin components freom greeter as dependency

--- src/main/java/vaadin/webcomponents/greeter/views/main/MainView.java
+++ src/main/java/vaadin/webcomponents/greeter/views/main/MainView.java
@@ -3,15 +3,18 @@ package vaadin.webcomponents.greeter.views.main;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.server.InitialPageSettings.WrapMode;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
@@ -40,8 +43,11 @@ import vaadin.webcomponents.greeter.views.main.greeter.GreeterComponent;
         enableInstallPrompt = false)
 public class MainView extends AppLayout {
    
-	private static final long serialVersionUID = -1817009361621321205L;
-
+        private static final long serialVersionUID = -1817009361621321205L;
+        
+        TextField textDependencyForGreeter = new TextField();
+        Button buttonDependencyForGreeter = new Button();
+       
 	public MainView() {
 
                 setDrawerOpened(false);
@@ -60,6 +66,7 @@ public class MainView extends AppLayout {
                 layout.addAndExpand(tabs);
 
                 var toggle = new DrawerToggle();
+                toggle.addClickListener(e -> Notification.show("text"));
                 toggle.setIcon(new Icon(VaadinIcon.CLIPBOARD_TEXT));
                 layout.add(toggle);
          
